eaee1b15c0a2cf3af0d7e13aff51c3560e70a2fc
Anders Olavessen <Anders Olavessen>
2016-04-22 17:39:21 -0500

    info done

--- app/router.js
+++ app/router.js
@@ -6,7 +6,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('games');
+  this.route('games', {path: '/'}, function() {
+    this.route('info');
+  });
 });
 
 export default Router;
