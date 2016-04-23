c64047436fe8ceb5ca08c9cfa49ae51dffaa6f61
Chris Boone <cboone1337@gmail.com>
2016-04-22 21:13:46 -0500

    refactor router addresses

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('games', {path: '/'}, function() {
+  this.route('games', function() {
     this.route('info');
   });
 });
